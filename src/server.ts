822d194c756a65c9d3f085f8bc49fb43bc29ea92
Eric Lee <ranye0719@gmail.com>
2019-05-04 14:39:54 +0800

    design Users api

--- src/server.ts
+++ src/server.ts
@@ -6,13 +6,37 @@ const { importSchema } = require('graphql-import');
 require('./db/db.ts');
 
 // Construct a schema, using GraphQL schema language
-const typeDefs = importSchema( __dirname + '/schemas/schema.graphql')
+const typeDefs = importSchema(__dirname + '/schemas/schema.graphql')
 
+const user = {
+  id: '1',
+  name: 'eric',
+  account: 'eric account',
+  password: 'eric password',
+  email: 'eric@email.com'
+};
 // The root provides a resolver function for each API endpoint
 const resolvers = {
   Query: {
     hello: (_, { name }) => `Hello ${name || 'World'}`,
+    getUser(preObj, args, context, info) {
+      return user;
+    },
+    getUsers(preObj, args, context, info) {
+      return [user];
+    }
   },
+  Mutation: {
+    createUser(preObj, args, context, info) {
+      return user;
+    },
+    updateUser(preObj, args, context, info) {
+      return user;
+    },
+    deleteUser(preObj, args, context, info) {
+      return user;
+    }
+  }
 };
 
 const server = new GraphQLServer({ typeDefs, resolvers });

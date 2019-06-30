8507c2e22634a1bfdf4055e0f7e6915e601373b6
Eric Lee <ranye0719@gmail.com>
2019-06-30 22:00:08 +0800

    mark auth

--- src/core/middleware.ts
+++ src/core/middleware.ts
@@ -1,17 +1,17 @@
 import { auth } from './auth';
 export const permissions = {
-  Query: {
-    getUser: auth,
-    getUsers: auth,
-    getPost: auth,
-    getPosts: auth,
-  },
-  Mutation: {
-    updateUser: auth,
-    deleteUser: auth,
-    createPost: auth,
-    updatePost: auth,
-    deletePost: auth,
-    logout: auth
-  }
+  // Query: {
+  //   getUser: auth,
+  //   getUsers: auth,
+  //   getPost: auth,
+  //   getPosts: auth,
+  // },
+  // Mutation: {
+  //   updateUser: auth,
+  //   deleteUser: auth,
+  //   createPost: auth,
+  //   updatePost: auth,
+  //   deletePost: auth,
+  //   logout: auth
+  // }
 }
